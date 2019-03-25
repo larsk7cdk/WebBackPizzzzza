014b7b450ac0b8b055453e19e77e5868b95cb645
Lars Larsen <lal@jyskebank.dk>
2019-03-25 15:11:58 +0100

    added unit test for basket service

---
+++ WebBackPizzzzza.UnitTests/Services/BasketServiceTest.cs
@@ -0,0 +1,107 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebBackPizzzzza.web.Services;
+
+namespace WebBackPizzzzza.UnitTests.Services
+{
+    [TestClass]
+    public class BasketServiceTest
+    {
+        private readonly IBasketService _sut = new BasketService();
+
+        [TestMethod]
+        public async Task AddToBasket_WhenAddOneItem_ExpectOneItemInBasket()
+        {
+            // Arrange
+            const int id = 1;
+            const int expected = 1;
+
+            // Act
+            await _sut.AddToBasket(id);
+
+            var result = await _sut.ProductsInBasket();
+            result.TryGetValue(id, out var actual);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task AddToBasket_WhenAddThreeItems_ExpectThreeItemsInBasket()
+        {
+            // Arrange
+            const int id = 2;
+            const int expected = 3;
+
+            // Act
+            await _sut.AddToBasket(id);
+            await _sut.AddToBasket(id);
+            await _sut.AddToBasket(id);
+
+            var result = await _sut.ProductsInBasket();
+            result.TryGetValue(id, out var actual);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task RemoveFromBasket_WhenAddThreeItemsRemoveOne_ExpectTwoItemsInBasket()
+        {
+            // Arrange
+            const int id = 3;
+            const int expected = 2;
+
+            // Act
+            await _sut.AddToBasket(id);
+            await _sut.AddToBasket(id);
+            await _sut.AddToBasket(id);
+            await _sut.RemoveFromBasket(id);
+
+            var result = await _sut.ProductsInBasket();
+            result.TryGetValue(id, out var actual);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task RemoveFromBasket_WhenAddOneItemRemoveThree_ExpectZeroItemInBasket()
+        {
+            // Arrange
+            const int id = 4;
+            const int expected = 0;
+
+            // Act
+            await _sut.AddToBasket(id);
+            await _sut.RemoveFromBasket(id);
+            await _sut.RemoveFromBasket(id);
+            await _sut.RemoveFromBasket(id);
+
+            var result = await _sut.ProductsInBasket();
+            result.TryGetValue(id, out var actual);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task RemoveFromBasket_WhenRemoveThree_ExpectZeroItemInBasket()
+        {
+            // Arrange
+            const int id = 5;
+            const int expected = 0;
+
+            // Act
+            await _sut.RemoveFromBasket(id);
+            await _sut.RemoveFromBasket(id);
+            await _sut.RemoveFromBasket(id);
+
+            var result = await _sut.ProductsInBasket();
+            result.TryGetValue(id, out var actual);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- WebBackPizzzzza.web/Services/BasketService.cs
+++ WebBackPizzzzza.web/Services/BasketService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebBackPizzzzza.web.ViewModels;
@@ -10,30 +12,31 @@ namespace WebBackPizzzzza.web.Services
         int NoOfProductsInBasket { get; }
         Task AddToBasket(int id);
         Task RemoveFromBasket(int id);
-        Task<IDictionary<int, int>> ProductsInBasket();
+        Task<ConcurrentDictionary<int, int>> ProductsInBasket();
     }
 
-    public class BasketService : IBasketService
+    public class BasketService : IBasketService, IDisposable
     {
-        private static readonly IDictionary<int, int> ProductsID = new Dictionary<int, int>();
+        private static ConcurrentDictionary<int, int> ProductsID = new ConcurrentDictionary<int, int>();
 
         int IBasketService.NoOfProductsInBasket => ProductsID.Count;
 
         public async Task AddToBasket(int id) =>
-            await Task.Run(() =>
-            {
-                if (ProductsID.TryGetValue(id, out var count)) ProductsID.Remove(id);
-                ProductsID.Add(id, count + 1);
-            });
+            await Task.FromResult(ProductsID.AddOrUpdate(id, 1, (key, oldValue) => oldValue + 1));
+
 
         public async Task RemoveFromBasket(int id) =>
-            await Task.Run(() =>
+            await Task.FromResult(ProductsID.AddOrUpdate(id, 1, (key, oldValue) =>
             {
-                if (ProductsID.TryGetValue(id, out var count)) ProductsID.Remove(id);
-                ProductsID.Add(id, count - 1);
-            });
+                var newValue = oldValue - 1;
+                return newValue <= 0 ? 0 : newValue;
+            }));
 
+        public Task<ConcurrentDictionary<int, int>> ProductsInBasket() => Task.FromResult(ProductsID);
 
-        public Task<IDictionary<int, int>> ProductsInBasket() => Task.FromResult(ProductsID);
+        public void Dispose()
+        {
+            ProductsID = null;
+        }
     }
 }
